246b118356674c7ca80ddaab9fa0973619edc402
Satyajit Dessai <satyajit_dessai@yahoo.in>
2016-10-11 16:24:22 +0530

    Current date seclection with image based dates

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -1,5 +1,7 @@
 var year = 2016;
-var month = 1;
+var month = 2;
+var currentday = 20;
+var currentmonth = 3;
 var weekday = null;
 var monthcolorcode = null;
 var calerdarxml = null;
@@ -38,12 +40,13 @@ function prevmonth(){
     }
 }
 
+var calcontentrowheight = 0;
 function updatecalendardata(){
     if(prevselmonth > 0){
         $('#calheader').removeClass("month"+ prevselmonth +"-theme");
-        $('#calheader').addClass("month"+ month +"-theme");
-    }
-    $('.tr-week').remove();
+    }    
+    $('#calheader').addClass("month"+ month +"-theme");
+    $('#caltb').find('tr').remove();
     var monthdatelist = $(calerdarxml).find("calendar[id='"+ year + "'] > month[id='"+month+"'] > date")
     var monthobj = $(calerdarxml).find("calendar[id='"+ year + "'] >  month[id='"+month+"']");
     $('.div-selected-month').text(function(i, oldText) {
@@ -51,22 +54,24 @@ function updatecalendardata(){
     });
     
      for(var i = 0 ; i< 5;i++){
-    $("#caltb").append('<tr class="tr-week"><td id="td0'+ i +'" width="13%"></td>     <td id="td1'+ i +'" width="13%"></td><td id="td2'+ i +'" width="13%"></td><td id="td3'+ i +'" width="13%"></td><td id="td4'+ i +'" width="13%"></td><td id="td5'+ i +'" width="13%"></td><td id="td6'+ i +'" width="13%"></td></tr>');
+    $("#caltb").append('<tr><td id="td0'+ i +'" width="13%" height="'+ calcontentrowheight +'px"></td>     <td id="td1'+ i +'" width="13%" height="'+ calcontentrowheight +'px"></td><td id="td2'+ i +'" width="13%" height="'+ calcontentrowheight +'px"></td><td id="td3'+ i +'" width="13%" height="'+ calcontentrowheight +'px"></td><td id="td4'+ i +'" width="13%" height="'+ calcontentrowheight +'px"></td><td id="td5'+ i +'" width="13%" height="'+ calcontentrowheight +'px"></td><td id="td6'+ i +'" width="13%" height="'+ calcontentrowheight +'px"></td></tr>');
     }
     
     var lastSelectedtd = null;
-    $(".tr-week > td").click(function() {
+    $("#caltb").find("td").click(function() {
         
-        var dateval = $(this).text();
+        var dateval = $(this).attr("id");
         if(!(dateval === undefined) && dateval != ""){
             $("#datedatacol").find("div").each(function(){
              $(this).remove();                              
             });
-            if(lastSelectedtd != null){   $(lastSelectedtd).removeAttr("style"); }
-            $(this).attr("style",'border-style: solid;border-color:' + monthcolorcode[month-1] +';border-width: 1px;');
+            if(lastSelectedtd != null && !(currentmonth == month && currentday == $(lastSelectedtd).attr("id"))){  $(lastSelectedtd).removeAttr("style"); }
+            if(!(currentmonth == month && currentday == dateval)){
+                $(this).attr("style",'border-style: solid;border-color:' + monthcolorcode[month-1] +';border-width: 1px;');
+            }
             lastSelectedtd = $(this);
             
-            var datalist = $(calerdarxml).find("calendar[id='"+ year + "'] > month[id='"+month+"'] > date[dn='"+ dateval +"'] > d")
+            var datalist = $(calerdarxml).find("calendar[id='"+ year + "'] > month[id='"+month+"'] > date[id='"+ dateval +"'] > d")
             $(datalist).each(function(){
                 var tagstr = $(this).attr("tag");
                 var divstyle = (!(jQuery.type(tagstr) === "undefined") && tagstr.indexOf("NAX") != -1) ? " style='color:green;'" : "";
@@ -84,23 +89,32 @@ function updatecalendardata(){
     $(monthdatelist).each(function(){
         var wkday = $(this).attr("wd");
         var cell = $("#td"+ wkday + y +"");
-        cell.text($(this).attr("dn"));
+        cell.append('<img src="img/mn/'+$(this).attr("id")+'.png" width="auto" height="60%"/>');
+        $(cell).attr("id",$(this).attr("id"));
         
+        if(currentmonth == month && currentday == $(this).attr("id")){
+            $(cell).attr("style",'background-color:' + monthcolorcode[month-1] +';');
+        }
         if(wkday == 6){ y += 1;}
         if(y == 5){ y = 0;}
         
     });   
 }
 
-function getcalfooterheight(calheaderid){
+function calculatedimensions(){
     var docht = $(document).height();
-    var calheaderht = $(calheaderid).height();
-    return docht - (calheaderht + 240 + 25); //documentheight -(calendar header height + calendar content height - buffer)
-}
-$( document ).on( "pageinit", "#maincalender", function() {
-    var footerheight = getcalfooterheight("#calheader");
+    var docwd = $(document).width();
+    var calheaderht = $("#calheader").height();
+    calcontentrowheight = (docwd * 13)/100;
+    var calcontentheight = (calcontentrowheight * 5) + 10;
+    $("#calcontent").height(calcontentheight);
+    var footerheight =  docht - (calheaderht + calcontentheight + 15); //documentheight -(calendar header height + calendar content height - buffer)
     $("#calfooter").height(footerheight);
     $("#datedatacol").height(footerheight - 80);
+}
+$( document ).on( "pageinit", "#maincalender", function() {
+    calculatedimensions();
+    
     loadCalendar();
 });
 
@@ -108,10 +122,13 @@ function loadCalendar(){
     if(calerdarxml != null && weekday != null){ return;  }
     
     monthcolorcode = new Array(12);
-    monthcolorcode[0]="#f6883d";monthcolorcode[1]="#f6887d";monthcolorcode[2]="#f6183d";monthcolorcode[3]="#16883d";
+    monthcolorcode[0]="#f6883d";monthcolorcode[1]="#f6887d";monthcolorcode[2]="#df88f0";monthcolorcode[3]="#37ddd0";
     monthcolorcode[4]="#f68c3d";monthcolorcode[5]="#f6c83d";monthcolorcode[6]="#f6183d";monthcolorcode[7]="#f6683d";
     monthcolorcode[8]="#16883d";monthcolorcode[9]="#46883d";monthcolorcode[10]="#86883d";monthcolorcode[11]="#d6883d";
-    
+    $('#calheader').addClass("month"+ month +"-theme");
+    $("#currentdate > img").remove();
+    $("#currentdate").attr("style",'background-color:' + monthcolorcode[currentmonth-1] +';');
+    $("#currentdate").append('<img src="img/mn/'+ currentday +'.png" width="auto" height="20%"/>');
     $("#prevcalmonth").click(function() {
         prevmonth();
     });
